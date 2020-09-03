b1766f5e3cd9a74fc10bd0ed45db9633510322ba
xiaoyonghu <2826755215@qq.com>
2020-09-03 09:00:16 +0800

    奇安信笔试

---
+++ src/main/java/com/huster/leetcode/qianxin/Exam001.java
@@ -0,0 +1,25 @@
+package com.huster.leetcode.qianxin;
+
+/**
+ * @author Created by Divo
+ * @date 2020/9/2
+ * Description: 青蛙跳
+ * Status:finished
+ */
+public class Exam001 {
+    public int numWays(int n) {
+        if (n < 0)
+            return 0;
+        if(n == 0)
+            return 1;
+        //f(n)= f(n-1)+f(n-2)
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2])%1000000007;
+        }
+        return dp[n];
+    }
+
+}
---
+++ src/main/java/com/huster/leetcode/qianxin/Exam002.java
@@ -0,0 +1,50 @@
+package com.huster.leetcode.qianxin;
+
+/**
+ * @author Created by Divo
+ * @date 2020/9/2
+ * Description: 分房子
+ * Status:new
+ */
+public class Exam002 {
+    public static int numWays(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int[] arr = new int[nums.length];
+        int res = 1;
+        arr[0] = 1;
+        int start = 0;
+        int min = Integer.MAX_VALUE;
+        //规则只考虑了后面和前面的
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                arr[i] = arr[i - 1] + 1;
+
+            } else if (nums[i] == nums[i - 1]) {
+                arr[i] = 1;
+
+                if (min < 1) {
+                    res += (1 - min) * (i - start);
+                }
+                //更新状态
+                min = Integer.MAX_VALUE;
+                start = i;
+            } else {
+                arr[i] = arr[i - 1] - 1;
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+            }
+            res += arr[i];
+        }
+        if (min < 1) {
+            res += (1 - min) * (nums.length - start);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] in = {3, 2, 4};
+        System.out.println(numWays(in));
+    }
+}
