3e5a50a3ad4097e26428cce4751239df910ac57a
Yongshao <2826755215@qq.com>
2022-09-14 09:48:00 +0800

    树的第一题

--- src/main/java/com/huster/leetcode/SwordToOffer/PartitionTwo/Test26.java
+++ src/main/java/com/huster/leetcode/SwordToOffer/PartitionTwo/Test26.java
@@ -10,7 +10,23 @@ import com.huster.leetcode.utils.TreeNode;
 public class Test26 {
 
     public boolean isSubStructure(TreeNode A, TreeNode B) {
-
-        return false;
+        //    A 与 B 都没有为空(还有节点)              以A、B(先序遍历)
+        return (A != null && B != null)
+                && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
     }
+
+    /**
+     * 以A、B为根节点是否匹配
+     *
+     * @param A
+     * @param B
+     * @return
+     */
+    private boolean recur(TreeNode A, TreeNode B) {
+        if (B == null) return true;
+        // B != null
+        if (A == null || A.val != B.val) return false;
+        return recur(A.left, B.left) && recur(A.right, B.right);
+    }
+
 }
---
+++ src/main/java/com/huster/leetcode/SwordToOffer/PartitionTwo/Test27.java
@@ -0,0 +1,58 @@
+package com.huster.leetcode.SwordToOffer.PartitionTwo;
+
+import com.huster.leetcode.utils.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * @Author yongshao
+ * @create 2022/9/13 23:25
+ * Description:  好久不刷树相关的题目了，太菜了
+ */
+public class Test27 {
+
+//    //dfs  由下而上
+//    public TreeNode mirrorTree(TreeNode root) {
+//
+//        if (root == null) return null;
+//        TreeNode tp = root.left;
+//        root.left = mirrorTree(root.right);
+//        root.right = mirrorTree(tp);
+//
+//        return root;
+//    }
+
+    //从上换到下
+//    public TreeNode mirrorTree(TreeNode root) {
+//
+//        if (root == null) return null;
+//        TreeNode tmp = root.left;
+//        root.left = root.right;
+//        root.right = tmp;
+//
+//        mirrorTree(root.left);
+//        mirrorTree(root.right);
+//
+//        return root;
+//    }
+
+    //使用栈来实现 从上而下
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) return null;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode t = stack.pop();
+            if (t.left != null) stack.add(t.left);
+            if (t.right != null) stack.add(t.right);
+            TreeNode tmp = t.left;
+            t.left = t.right;
+            t.right = tmp;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
